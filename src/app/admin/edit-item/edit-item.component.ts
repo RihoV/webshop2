a96f56253507e9f702c0aee0fd3285983acad077
RihoV <76494236+RihoV@users.noreply.github.com>
2021-10-05 12:27:34 +0300

    routes

--- src/app/admin/edit-item/edit-item.component.ts
+++ src/app/admin/edit-item/edit-item.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditItemComponent implements OnInit {
 
+  id!: any;
+  item!: any;
+
   constructor() { }
 
   ngOnInit(): void {
